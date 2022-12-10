ddef15cee193c5ad1506901f759414911ddb327b
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-10 21:51:24 +0900

    feat:static site 생성

--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,32 @@
 export const SITE = {
-	title: 'Documentation',
-	description: 'Your website description.',
-	defaultLanguage: 'en_US',
+  title: "mini-board Wiki",
+  description: "mini board team wiki",
+  defaultLanguage: "en_US",
 };
 
 export const OPEN_GRAPH = {
-	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true',
-		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
-	},
-	twitter: 'astrodotbuild',
+  image: {
+    src: "https://github.com/withastro/astro/blob/main/assets/social/banner-minimal.png?raw=true",
+    alt:
+      "astro logo on a starry expanse of space," +
+      " with a purple saturn-like planet floating in the right foreground",
+  },
+  twitter: "astrodotbuild",
 };
 
 // This is the type of the frontmatter you put in the docs markdown files.
 export type Frontmatter = {
-	title: string;
-	description: string;
-	layout: string;
-	image?: { src: string; alt: string };
-	dir?: 'ltr' | 'rtl';
-	ogLocale?: string;
-	lang?: string;
+  title: string;
+  description: string;
+  layout: string;
+  image?: { src: string; alt: string };
+  dir?: "ltr" | "rtl";
+  ogLocale?: string;
+  lang?: string;
 };
 
 export const KNOWN_LANGUAGES = {
-	English: 'en',
+  Korean: "ko",
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
@@ -36,22 +36,33 @@ export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
-	indexName: 'XXXXXXXXXX',
-	appId: 'XXXXXXXXXX',
-	apiKey: 'XXXXXXXXXX',
+  indexName: "XXXXXXXXXX",
+  appId: "XXXXXXXXXX",
+  apiKey: "XXXXXXXXXX",
 };
 
 export type Sidebar = Record<
-	typeof KNOWN_LANGUAGE_CODES[number],
-	Record<string, { text: string; link: string }[]>
+  typeof KNOWN_LANGUAGE_CODES[number],
+  Record<string, { text: string; link: string }[]>
 >;
 export const SIDEBAR: Sidebar = {
-	en: {
-		'Section Header': [
-			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
-		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
-	},
+  ko: {
+    '': [
+      { text: "소개글", link: "introduction" },
+    ],
+    Rules: [
+      {	text: "규칙", link: "팀-규칙"	},
+
+      {	text: "커밋", link: "커밋-컨벤션"	},
+      {	text: "코드", link: "코드-컨벤션"	},
+    ],
+    "Scrum log": [
+	  { text: "킥오프", link: "킥오프" },
+      { text: "22-12-05", link: "22-12-05" },
+      { text: "22-12-06", link: "22-12-06" },
+      { text: "22-12-07", link: "22-12-07" },
+	  { text: "22-12-08", link: "22-12-08" },
+      { text: "22-12-09", link: "22-12-09" },
+    ],
+  },
 };
---
+++ src/pages/introduction.md
@@ -0,0 +1,8 @@
+---
+layout: ../layouts/MainLayout.astro
+---
+# introduction
+**이 위키의  `목적`은 서로 협의한 사항을 기억하기 위함입니다.**
+### 바로가기
+- [깃허브 레포](https://github.com/chloeeee210/mini-board)
+- [figma 페이지](https://www.figma.com/file/OBqZSHpWFugC2CITAnTjNy/Untitled?node-id=0%3A1)
